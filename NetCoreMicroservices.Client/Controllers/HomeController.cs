376ab356a9ab650e23fb06f5b385a5338d86b455
patrikduch <duchpatrik@icloud.com>
2021-03-14 23:52:59 +0100

    ClientApp: Comments fix

--- NetCoreMicroservices.Client/Controllers/HomeController.cs
+++ NetCoreMicroservices.Client/Controllers/HomeController.cs
@@ -2,23 +2,30 @@
 using Microsoft.Extensions.Logging;
 using NetCoreMicroservices.Client.Data;
 using NetCoreMicroservices.Client.Models;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace NetCoreMicroservices.Client.Controllers
 {
+    /// <summary>
+    /// MVC controller for root page.
+    /// </summary>
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
 
+        /// <summary>
+        /// Default constructor for HomeController
+        /// </summary>
+        /// <param name="logger"></param>
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
+        /// <summary>
+        /// Index page of the app
+        /// </summary>
+        /// <returns></returns>
         public IActionResult Index()
         {
             return View(ProductContext.Products);
