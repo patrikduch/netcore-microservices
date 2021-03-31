0f54eaaab68b575d02a3739a2830e9a6b9526a10
patrikduch <duchpatrik@icloud.com>
2021-03-31 23:39:02 +0200

    Net: changing presentation of WebMVC client

--- NetCoreMicroservices.Client/Models/Product.cs
+++ NetCoreMicroservices.Client/Models/Product.cs
@@ -1,10 +1,15 @@
-﻿namespace NetCoreMicroservices.Client.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetCoreMicroservices.Client.Models
 {
     public class Product
     {
         public string Id { get; set; }
+        [Display(Name="Název")]
         public string Name { get; set; }
+        [Display(Name = "Kategorie")]
         public string Category { get; set; }
+        [Display(Name = "Popis")]
         public string Description { get; set; }
         public string ImageFile { get; set; }
         public decimal Price { get; set; }
