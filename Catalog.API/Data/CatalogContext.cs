166307b5afd341eb8354c3027a087ec0b10343f1
patrikduch <duchpatrik@icloud.com>
2021-04-20 23:41:33 +0200

    Docker: CatalogAPI integration into docker-compose setup

---
+++ Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,6 @@
+﻿namespace Catalog.API.Data
+{
+    public class CatalogContext
+    {
+    }
+}
---
+++ Catalog.API/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+
+        public string Summary { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageFile { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
