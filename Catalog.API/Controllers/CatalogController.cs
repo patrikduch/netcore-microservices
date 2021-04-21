c5bfe053deea098cab33e101bb59e19347f220f1
patrikduch <duchpatrik@icloud.com>
2021-04-21 12:04:16 +0200

    Net: Catalog API REST controller

---
+++ Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,34 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Repositiories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/{controller}")]
+    public class CatalogController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        private readonly ILogger<CatalogController> _logger;
+
+        public CatalogController(IProductRepository productRepository, ILogger<CatalogController> logger)
+        {
+            _logger = logger;
+            _productRepository = productRepository;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Product>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProducts();
+            return Ok(products);
+        }
+
+    }
+}
