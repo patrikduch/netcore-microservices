40447c622f8f0542d98c71c13319b18017939ce4
patrikduch <duchpatrik@icloud.com>
2021-04-23 11:58:41 +0200

    NET: DiscountAPI - basic repository setup

---
+++ Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,19 @@
+﻿using Discount.API.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    /// <summary>
+    /// Contract for data repository <seealso cref="DiscountRepository"/>.
+    /// </summary>
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+
+        Task<bool> CreateDiscount(Coupon coupon);
+
+        Task<bool> UpdateDiscount(Coupon coupon);
+
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
