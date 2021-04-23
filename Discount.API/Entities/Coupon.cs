dd304ab531bcd3e00c0a263c76517e905fac5120
patrikduch <duchpatrik@icloud.com>
2021-04-23 07:07:17 +0200

    NET: Coupon entity for DiscountAPI service

---
+++ Discount.API/Entities/Coupon.cs
@@ -0,0 +1,13 @@
+﻿namespace Discount.API.Entities
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
