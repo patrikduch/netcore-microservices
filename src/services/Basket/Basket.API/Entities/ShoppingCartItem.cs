b9b78620004326eaf2c99dca049bc7fb6d39d56d
patrikduch <duchpatrik@icloud.com>
2021-05-03 21:18:58 +0200

    NET: BasketAPI: ShoppingCartITem properties comment fix

--- src/services/Basket/Basket.API/Entities/ShoppingCartItem.cs
+++ src/services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -2,14 +2,29 @@
 {
     public class ShoppingCartItem
     {
+        /// <summary>
+        /// Gets or sets quantity of shoppingcart item.
+        /// </summary>
         public int Quantity { get; set; }
 
+        /// <summary>
+        /// Gets or sets color of shoppingcart item.
+        /// </summary>
         public string Color { get; set; }
 
+        /// <summary>
+        /// Gets or sets price of shoppingcart item.
+        /// </summary>
         public decimal Price { get; set; }
 
+        /// <summary>
+        /// Gets or sets product identifier of shoppingcart item.
+        /// </summary>
         public string ProductId { get; set; }
 
+        /// <summary>
+        /// Gets or sets product name  of shoppingcart item.
+        /// </summary>
         public string ProductName { get; set; }
     }
 }
