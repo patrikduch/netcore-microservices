9714d73b79b166a99a4d20e5dd5adc8992475d82
patrikduch <duchpatrik@icloud.com>
2021-05-03 20:56:13 +0200

    NET: Coupon properties comment fix

--- src/services/Discount/Discount.Grpc/Entities/Coupon.cs
+++ src/services/Discount/Discount.Grpc/Entities/Coupon.cs
@@ -2,12 +2,24 @@
 {
     public class Coupon
     {
+        /// <summary>
+        /// Gets or sets coupon's identifier.
+        /// </summary>
         public int Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets product's name for particular coupon.
+        /// </summary>
         public string ProductName { get; set; }
 
+        /// <summary>
+        /// Gets or sets product's description for particular coupon.
+        /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// Gets or sets amounth for particular coupon.
+        /// </summary>
         public int Amount { get; set; }
     }
 }
