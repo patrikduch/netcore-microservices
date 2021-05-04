0a6625aa622a459874a892f66992e7bc5b426c29
patrikduch <duchpatrik@icloud.com>
2021-05-04 15:57:01 +0200

    NET: CatalogAPI - SignalR testing

---
+++ src/services/Catalog/Catalog.API/Hubs/ViewHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Hubs
+{
+    public class ViewHub : Hub
+    {
+        public static int ViewCount { get; set; } = 0;
+
+        public async Task NotifyWatching(int uId, string token)
+        {
+            await Clients.All.SendAsync("data", new { uId, token = token, });
+        }
+
+    }
+}
