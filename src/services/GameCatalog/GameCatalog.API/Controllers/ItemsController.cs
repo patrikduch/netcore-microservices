90c4412d54e1f7f8a91ea662d9466e324da1f5e1
patrikduch <duchpatrik@icloud.com>
2021-05-04 09:30:08 +0200

    NET: GameCatalog.API- Read REST actions

---
+++ src/services/GameCatalog/GameCatalog.API/Controllers/ItemsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameCatalog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private static readonly List<ItemDto> _items = new()
+        {
+            new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of  HP", 5, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Bronze sword", "Deals a small amout of damage", 20, DateTimeOffset.UtcNow),
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
+        {
+            return _items;
+        }
+
+        // GET /items/{id}
+        [HttpGet("{id}")]
+        public ItemDto GetById(Guid id)
+        {
+            var item = _items.Where(i => i.Id == id).SingleOrDefault();
+            return item;
+        }
+    }
+}
---
+++ src/services/GameCatalog/GameCatalog.API/Dtos.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GameCatalog.API
+{
+    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
+
+    public record CreateItemDto(string Name, string Description, decimal Price);
+
+    public record UpdateItemDto(string Name, string Description, decimal Price);
+}
