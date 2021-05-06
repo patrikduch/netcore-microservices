1bf94b6c401d405bb28e8f350e7a712bab2fa43f
patrikduch <duchpatrik@icloud.com>
2021-05-07 01:11:21 +0200

    NET: GameCatalog.API- comments fix

--- src/services/GameCatalog/GameCatalog.API/Entities/Item.cs
+++ src/services/GameCatalog/GameCatalog.API/Entities/Item.cs
@@ -2,15 +2,34 @@
 
 namespace GameCatalog.API.Entities
 {
+    /// <summary>
+    /// Entity for representation gamecatalog item.
+    /// </summary>
     public class Item
     {
+        /// <summary>
+        /// Gets or sets item entity identifier.
+        /// </summary>
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Gets or sets item's name.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Gets or sets item's description.
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets item's price.
+        /// </summary>
         public decimal Price { get; set; }
 
+        /// <summary>
+        /// Gets or sets item's created date-
+        /// </summary>
         public DateTimeOffset CreatedDate { get; set; }
     }
 }
