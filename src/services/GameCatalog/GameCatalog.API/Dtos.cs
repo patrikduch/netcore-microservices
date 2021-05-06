f46f78efa1b237d63d0c04fb58b1210d414a65a3
patrikduch <duchpatrik@icloud.com>
2021-05-06 09:34:07 +0200

    NET: GameCatalog.API - dtos validation

--- src/services/GameCatalog/GameCatalog.API/Dtos.cs
+++ src/services/GameCatalog/GameCatalog.API/Dtos.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameCatalog.API
 {
     public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
 
-    public record CreateItemDto(string Name, string Description, decimal Price);
+    public record CreateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
 
-    public record UpdateItemDto(string Name, string Description, decimal Price);
+    public record UpdateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
 }
