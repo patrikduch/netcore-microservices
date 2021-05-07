2f191868fb21757af917830ba68509c074d89cf0
patrikduch <duchpatrik@icloud.com>
2021-05-07 21:07:23 +0200

    AKS: GameCatalog.API secrets fix

--- src/services/GameCatalog/GameCatalog.API/Settings/MongoDbSettings.cs
+++ src/services/GameCatalog/GameCatalog.API/Settings/MongoDbSettings.cs
@@ -21,6 +21,16 @@
         public string Host { get; init; }
 
         /// <summary>
+        /// Gets username for connection to the Mongodb.
+        /// </summary>
+        public string Username { get; set; }
+
+        /// <summary>
+        /// Gets password for connection to the Mongodb. 
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
         /// Gets port of particular Mongodb.
         /// </summary>
         public string Port { get; init; }
@@ -28,6 +38,16 @@
         /// <summary>
         /// Get connection string that is determinated from properties "Host" and "Port".
         /// </summary>
-        public string ConnectionString => $"mongodb://{Host}:{Port}";
+        public string ConnectionString { 
+            get {
+
+                if (Username == null|| Password == null )
+                {
+                    return $"mongodb://{Host}:{Port}";
+                }
+
+                return $"mongodb://{Username}:{Password}@{Host}:{Port}";
+            } 
+        }
     }
 }
