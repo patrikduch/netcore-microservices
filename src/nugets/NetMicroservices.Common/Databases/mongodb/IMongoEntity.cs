56af70a83923416f05b47e3c1f922687da815daa
patrikduch <duchpatrik@icloud.com>
2021-05-09 08:51:47 +0200

    NET: Common nuget - Basic MongoDb interfaces

---
+++ src/nugets/NetMicroservices.Common/Databases/mongodb/IMongoEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetMicroservices.Common.Databases.Mongodb
+{
+    /// <summary>
+    /// Contract for all Mongodb entities.
+    /// </summary>
+    public interface IMongoEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ src/nugets/NetMicroservices.Common/Databases/mongodb/IMongoRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NetMicroservices.Common.Databases.Mongodb
+{
+    /// <summary>
+    /// Interface for all mongodb repositories.
+    /// </summary>
+    /// <typeparam name="T">Type of mongodb entity.</typeparam>
+    public interface IMongoRepository<T> where T: IMongoEntity
+    {
+        Task CreateAsync(T entity);
+        Task RemoveAsync(Guid id);
+        Task<T> GetAsync(Guid id);
+        Task<IReadOnlyCollection<T>> GetAllAsync();
+        Task UpdateAsync(T entity);
+    }
+}
