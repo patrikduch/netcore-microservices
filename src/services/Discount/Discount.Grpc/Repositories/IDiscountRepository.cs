615e8fdd929092c10f9f13b6acba932a41b16857
patrikduch <duchpatrik@icloud.com>
2021-05-15 10:14:43 +0200

    NET: Discount.GRPC - spacing fix

--- src/services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
+++ src/services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
@@ -9,11 +9,8 @@ namespace Discount.Grpc.Repositories
     public interface IDiscountRepository
     {
         Task<Coupon> GetDiscount(string productName);
-
         Task<bool> CreateDiscount(Coupon coupon);
-
         Task<bool> UpdateDiscount(Coupon coupon);
-
         Task<bool> DeleteDiscount(string productName);
     }
 }
