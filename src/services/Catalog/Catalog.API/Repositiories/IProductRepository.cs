a23fd39fa0f18010da86a0505ef8d67782112022
patrikduch <duchpatrik@icloud.com>
2021-05-15 08:49:56 +0200

    NET: Catalog.API spacing fix

--- src/services/Catalog/Catalog.API/Repositiories/IProductRepository.cs
+++ src/services/Catalog/Catalog.API/Repositiories/IProductRepository.cs
@@ -10,17 +10,11 @@ namespace Catalog.API.Repositiories
     public interface IProductRepository
     {
         Task<IEnumerable<Product>> GetProducts();
-
         Task<Product> GetProduct(string id);
-
         Task<IEnumerable<Product>> GetProductByName(string name);
-
         Task<IEnumerable<Product>> GetProductByCategory(string categoryName);
-
         Task CreateProduct(Product product);
-
         Task<bool> UpdateProduct(Product product);
-
         Task<bool> DeleteProduct(string id);
     }
 }
