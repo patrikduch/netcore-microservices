41676667869c2d7427bd2124bdc8a4f59627928d
patrikduch <duchpatrik@icloud.com>
2021-05-16 02:52:54 +0200

    NET: MongoWrapper nuget test deployment of a new version

---
+++ src/nugets/MongoDbWrapper.Nuget/IMongoEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MongoDbWrapper.Nuget
+{
+    /// <summary>
+    /// Contract for all Mongodb entities.
+    /// </summary>
+    public interface IMongoEntity
+    {
+        /// <summary>
+        /// Get or set mongo entity identifier.
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+}
