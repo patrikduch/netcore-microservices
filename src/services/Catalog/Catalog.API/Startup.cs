6f1a6113aa92743765ba1414b40bfa4d826cf08e
patrikduch <duchpatrik@icloud.com>
2021-05-20 00:52:08 +0200

    NET: Catalog.API - service cleanup

--- src/services/Catalog/Catalog.API/Startup.cs
+++ src/services/Catalog/Catalog.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using MongoDB.Driver;
 using NetMicroservices.MongoDbWrapper;
+using NetMicroservices.MongoDbWrapper.Settings;
 using System;
 using System.Collections.Generic;
 
