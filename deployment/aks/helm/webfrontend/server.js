d496cf21cec992019a7eb9790a54951eff3c5118
patrikduch <duchpatrik@icloud.com>
2021-05-23 19:44:02 +0200

    HELM: Node.js app example

---
+++ deployment/aks/helm/webfrontend/server.js
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/api', function (req, res) {
+    res.send('Tomáš Silber čumí jak puk...');
+});
+
+var port = process.env.PORT || 80;
+var server = app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
+
+process.on("SIGINT", () => {
+    process.exit(130 /* 128 + SIGINT */);
+});
+
+process.on("SIGTERM", () => {
+    console.log("Terminating...");
+    server.close();
+});
