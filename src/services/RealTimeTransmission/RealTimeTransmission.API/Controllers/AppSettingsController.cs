b63e60cf5b6aefb3e7a5645fd553e182c905c1bc
patrikduch <duchpatrik@icloud.com>
2021-05-23 12:08:59 +0200

    NET: RealTimeTransmission.API - Appsettings controller

---
+++ src/services/RealTimeTransmission/RealTimeTransmission.API/Controllers/AppSettingsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RealTimeTransmission.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AppSettingsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult GetAppSettings()
+        {
+            return Ok(new { name = "RealTimeTransmission.API" });
+
+        }
+    }
+}
