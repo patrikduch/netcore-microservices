555826fccb467c094f9199fa9ccb419e3cce17c1
patrikduch <duchpatrik@icloud.com>
2021-05-27 09:40:34 +0200

    NET: RealTimeTransmission removal of unnecessary message handler

--- src/services/RealTimeTransmission/RealTimeTransmission.API/MessageHandlers/UserHandler.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace RealTimeTransmission.API.MessageHandlers
-{
-    public static class UserHandler
-    {
-        public static HashSet<string> ConnectedIds = new HashSet<string>();
-    }
-}
