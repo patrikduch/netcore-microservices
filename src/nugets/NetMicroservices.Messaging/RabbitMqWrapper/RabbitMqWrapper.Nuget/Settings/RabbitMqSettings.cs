9f42ff60e86b57c0e6920de5f24926b0b0a62e99
patrikduch <duchpatrik@icloud.com>
2021-06-05 10:37:47 +0200

    NET: RabbitMQ wrapper nuget change of framework version to NET5

--- src/nugets/NetMicroservices.Messaging/RabbitMqWrapper/RabbitMqWrapper.Nuget/Settings/RabbitMqSettings.cs
+++ src/nugets/NetMicroservices.Messaging/RabbitMqWrapper/RabbitMqWrapper.Nuget/Settings/RabbitMqSettings.cs
@@ -8,6 +8,6 @@
         /// <summary>
         /// Host name of RabbitMQ service bus.
         /// </summary>
-        public string Host { get; set; }
+        public string Host { get; init; }
     }
 }
