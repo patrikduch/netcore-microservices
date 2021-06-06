aec94d655834dad7130a0ca34bfb2c0609f135f8
patrikduch <duchpatrik@icloud.com>
2021-06-06 21:05:45 +0200

    NET: WEb.API Gateway production setup

--- src/services/API-Gws/Web-GW/Startup.cs
+++ src/services/API-Gws/Web-GW/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Kubernetes;
 
 namespace Web_GW
 {
@@ -13,7 +14,8 @@ namespace Web_GW
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddOcelot();
+            services.AddOcelot()
+                .AddKubernetes(); ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
