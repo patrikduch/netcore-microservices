ad14f4c1126c1b342b4fc8267646d9a7835d57d1
patrikduch <duchpatrik@icloud.com>
2021-06-09 11:15:08 +0200

    NET: WEBGW inventory test route

--- src/services/Inventory/Inventory.API/Controllers/ItemsController.cs
+++ src/services/Inventory/Inventory.API/Controllers/ItemsController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace Inventory.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ItemsController : ControllerBase
     {
