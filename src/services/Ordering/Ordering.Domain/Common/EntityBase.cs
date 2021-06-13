4b43f009939cc5dc174c02ae30aca531d53d3ccb
patrikduch <duchpatrik@icloud.com>
2021-06-13 07:44:07 +0200

    NET: Ordering microservice initialization

---
+++ src/services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ordering.Domain.Common
+{
+    /// <summary>
+    /// Common fields for all SqlServer entities.
+    /// </summary>
+    public abstract class EntityBase
+    {
+        public int Id { get; protected set; }
+
+        public string CreatedBy { get; set; }
+            
+        public DateTime CreatedAt { get; set; }
+
+        public string LastModifiedBy { get; set; }
+
+        public DateTime? LastModifiedDate { get; set; }
+    }
+}
---
+++ src/services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -0,0 +1,27 @@
+﻿using Ordering.Domain.Common;
+
+namespace Ordering.Domain.Entities
+{
+    public class Order : EntityBase
+    {
+        public string Username { get; set; }
+        public decimal TotalPrice { get; set; }
+
+        // Billing Address
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string AddressLine { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+
+        // Payment
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string Expiration { get; set; }
+        public string CVV { get; set; }
+        public int PaymentMethod { get; set; }
+    }
+}
