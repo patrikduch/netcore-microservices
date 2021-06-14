fa5fd281ddd5c4ff478190f09c0a961a17b1444a
patrikduch <duchpatrik@icloud.com>
2021-06-14 19:03:06 +0200

    NET: SqlWrapper asyncrepository contract

--- src/nugets/NetMicroservices.Dbs/NetMicroservices.SqlWrapper/NetMicroservices.SqlWrapper.Nuget/Contracts/IAsyncRepository.cs
+++ src/nugets/NetMicroservices.Dbs/NetMicroservices.SqlWrapper/NetMicroservices.SqlWrapper.Nuget/Contracts/IAsyncRepository.cs
@@ -1,13 +1,10 @@
-﻿using NetMicroservices.SqlWrapper.Nuget;
-using Ordering.Domain.Common;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace Ordering.Application.Contacts.Persistence
+namespace NetMicroservices.SqlWrapper.Nuget
 {
     /// <summary>
     /// Contract for all RDBMS  entities.
