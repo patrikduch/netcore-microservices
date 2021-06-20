4e87e66101d9596c19fc096de20db409944b7287
patrikduch <duchpatrik@icloud.com>
2021-06-20 10:27:38 +0200

    React: AppRoute component cleanup

--- src/webapps/Web.Spa/ClientApp/components/shared/AppRoute.tsx
+++ src/webapps/Web.Spa/ClientApp/components/shared/AppRoute.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Route, RouteProps} from "react-router";
+import { Route, RouteProps } from "react-router";
 
 export interface IProps extends RouteProps {
     layout: React.ComponentClass<any>;
