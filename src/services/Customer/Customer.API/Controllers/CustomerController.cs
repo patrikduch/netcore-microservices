624d91b8bfe5f5921a07d6eb2fbc47bbbc28c1b2
patrikduch <duchpatrik@icloud.com>
2021-06-21 09:30:49 +0200

    NET: Customer microservices - DbMigration refactor

--- src/services/Customer/Customer.API/Controllers/CustomerController.cs
+++ src/services/Customer/Customer.API/Controllers/CustomerController.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Customer.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class CustomerController : ControllerBase
     {
