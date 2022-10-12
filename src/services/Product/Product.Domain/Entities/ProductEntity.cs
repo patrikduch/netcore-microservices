39344d10a1a06571a805bdcbc0fda2273d11111b
patrikduch <duchpatrik@icloud.com>
2022-10-12 09:12:42 +0200

    Product microservice - initialization

---
+++ src/services/Product/Product.Domain/Entities/ProductEntity.cs
@@ -0,0 +1,32 @@
+﻿namespace Product.Domain.Entities;
+
+/// <summary>
+/// Entity that represents set of products.
+/// </summary>
+public class ProductEntity
+{
+    /// <summary>
+    /// Gets or sets product identifier.
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets product name.
+    /// </summary>
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets product description.
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets product imageUrl.
+    /// </summary>
+    public string ImgUrl { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets product price.
+    /// </summary>
+    public decimal Price { get; set; }
+}
