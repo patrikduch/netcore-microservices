057d64064a68c2f6dc94f57bc2a0747fe0c4d955
patrikduch <duchpatrik@icloud.com>
2022-10-13 08:49:42 +0200

    Web.Blazor - Product list basic HTML structure

--- src/webapps/Web.Blazor/Shared/Product.cs
+++ src/webapps/Web.Blazor/Shared/Product.cs
@@ -5,4 +5,10 @@ public class Product
     public int Id { get; set; }
 
     public string Name { get; set; } = string.Empty;
+
+    public string ImgUrl { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public decimal Price { get; set; }
 }
