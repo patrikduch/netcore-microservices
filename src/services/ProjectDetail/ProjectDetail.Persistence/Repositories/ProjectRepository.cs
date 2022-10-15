e8ef5add83eddec429929ae707da7cefc9f1db5e
patrikduch <duchpatrik@icloud.com>
2022-10-15 12:28:35 +0200

    ProjectDetail microservice - DI dependency naming fix

--- src/services/ProjectDetail/ProjectDetail.Persistence/Repositories/ProjectRepository.cs
+++ src/services/ProjectDetail/ProjectDetail.Persistence/Repositories/ProjectRepository.cs
@@ -2,27 +2,20 @@
 using ProjectDetail.Application.Contracts.Persistence;
 using ProjectDetail.Domain;
 using ProjectDetail.Persistence.Contexts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjectDetail.Persistence.Repositories
 {
     public class ProjectRepository : RepositoryBase<Project, ProjectContext>, IProjectRepository
     {
-
         private readonly ProjectContext _projectContext;
 
         /// <summary>
         /// Initializes a new instance of the <seealso cref="ProjectRepository"/>.
         /// </summary>
         /// <param name="projectContext">Order <seealso cref="DbContext"/> dependency object.</param>
-        public ProjectRepository(ProjectContext orderContext) : base(orderContext)
+        public ProjectRepository(ProjectContext projectContext) : base(projectContext)
         {
-            _projectContext = orderContext;
+            _projectContext = projectContext;
         }
-
     }
 }
