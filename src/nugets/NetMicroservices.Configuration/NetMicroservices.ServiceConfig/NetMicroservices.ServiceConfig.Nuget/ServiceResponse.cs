bdee366ac2d96b86919bbc6f717f92e1072d6aed
patrikduch <duchpatrik@icloud.com>
2022-10-19 08:05:08 +0200

    ServiceConfig.Nuget - ServiceResponse dto

---
+++ src/nugets/NetMicroservices.Configuration/NetMicroservices.ServiceConfig/NetMicroservices.ServiceConfig.Nuget/ServiceResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace NetMicroservices.ServiceConfig.Nuget;
+
+public class ServiceResponse<T>
+{
+    public T Data { get; set; }
+
+    public bool Success { get; set; } = true;
+
+    public string Message { get; set; } = string.Empty;
+}
