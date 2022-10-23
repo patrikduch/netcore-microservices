d941a292d9c6bd84e5aaa009220c87ec32febd4d
patrikduch <duchpatrik@icloud.com>
2022-10-23 19:35:17 +0200

    Web.Blazor - Register page

--- src/webapps/Web.Blazor/Shared/UserRegister.cs
+++ src/webapps/Web.Blazor/Shared/UserRegister.cs
@@ -1,6 +1,16 @@
-﻿namespace Web.Blazor.Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Blazor.Shared;
 
 public class UserRegister
 {
-    
+    [Required]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, StringLength(100, MinimumLength = 6)]
+    public string Password { get; set; } = string.Empty;
+
+    [Compare("Password", ErrorMessage = "The passwords do not match")]
+    public string ConfirmPassword { get; set; } = string.Empty;
 }
+
