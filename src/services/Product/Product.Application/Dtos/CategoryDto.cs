8847ab2a906c886b4d24614a2e206e8eeb30fd5e
patrikduch <duchpatrik@icloud.com>
2022-10-23 12:36:19 +0200

    Product.API -  Comments fix

--- src/services/Product/Product.Application/Dtos/CategoryDto.cs
+++ src/services/Product/Product.Application/Dtos/CategoryDto.cs
@@ -4,7 +4,6 @@
 // </copyright>
 // <author>Patrik Duch</author>
 //---------------------------------------------------------------------------
-
 namespace Product.Application.Dtos;
 
 /// <summary>
