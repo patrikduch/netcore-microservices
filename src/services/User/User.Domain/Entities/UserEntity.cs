ff92e6323cab1ca3496e38f6e5ca08bdf561357d
patrikduch <duchpatrik@icloud.com>
2022-10-30 13:29:57 +0100

    AKS: Project-Detail replicaset increse

---
+++ src/services/User/User.Domain/Entities/UserEntity.cs
@@ -0,0 +1,14 @@
+﻿using NetMicroservices.SqlWrapper.Nuget;
+
+namespace User.Domain.Entities;
+
+public class UserEntity : EntityBase
+{
+    public Guid Id { get; set; }
+
+    public string Email { get; set; } = string.Empty;
+
+    public byte[] PasswordHash { get; set; }
+
+    public byte[] PasswordSalt { get; set; }
+}
---
+++ src/services/User/User.Persistence/Contexts/UserContext.cs
@@ -0,0 +1,18 @@
+﻿//---------------------------------------------------------------------------
+// <copyright file="UserContext.cs" website="Patrikduch.com">
+//     Copyright (c) Patrik Duch, IČ: 09225471
+// </copyright>
+// <author>Patrik Duch</author>
+//---------------------------------------------------------------------------
+namespace User.Persistence.Contexts;
+
+using Microsoft.EntityFrameworkCore;
+using User.Domain.Entities;
+
+/// <summary>
+/// <seealso cref="DbContext"/>  configuration for  <seealso cref="UserEntity"/>.
+/// </summary>
+public class UserContext : DbContext
+{
+    
+}
