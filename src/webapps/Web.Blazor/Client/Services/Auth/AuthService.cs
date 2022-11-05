2d96876dfd3321556860b9a36526ee20399c3f9b
patrikduch <duchpatrik@icloud.com>
2022-11-05 11:36:07 +0100

    Web.Blazor - Registration view

---
+++ src/webapps/Web.Blazor/Client/Services/Auth/AuthService.cs
@@ -0,0 +1,24 @@
+﻿namespace Web.Blazor.Client.Services.Auth;
+
+using NetMicroservices.ServiceConfig.Nuget;
+using Web.Blazor.Shared;
+using System.Net.Http.Json;
+
+public class AuthService : IAuthService
+{
+    private readonly HttpClient _httpClient;
+    
+    public AuthService(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+    }
+    
+    public async Task<ServiceResponse<Guid>> Register(UserRegister request)
+    {
+        var response = await _httpClient.PostAsJsonAsync("/user-register", request);
+        
+        var result = await response.Content.ReadFromJsonAsync<ServiceResponse<Guid>>();
+
+        return result;
+    }
+}
---
+++ src/webapps/Web.Blazor/Client/Services/Auth/IAuthService.cs
@@ -0,0 +1,9 @@
+﻿namespace Web.Blazor.Client.Services.Auth;
+
+using NetMicroservices.ServiceConfig.Nuget;
+using Web.Blazor.Shared;
+
+public interface IAuthService
+{
+    Task<ServiceResponse<Guid>> Register(UserRegister request);
+}
