0934fba4f018ecbe7e93e217b7a28671adaa2edc
patrikduch <duchpatrik@icloud.com>
2022-12-16 10:53:13 +0100

    Product.API -  EFCore migration: cleanup product data

---
+++ src/services/Product/Product.Persistence/Migrations/20221216094617_ProductEntityReset.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Product.Persistence.Migrations
+{
+    public partial class ProductEntityReset : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("TRUNCATE TABLE public.product;");
+            migrationBuilder.Sql("DELETE FROM public.product;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
