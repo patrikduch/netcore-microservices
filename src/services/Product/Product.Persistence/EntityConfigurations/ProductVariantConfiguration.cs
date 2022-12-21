256f794629b6876ea0e189d21f0a0243dd5e60c2
patrikduch <duchpatrik@icloud.com>
2022-12-21 20:26:39 +0100

    Product.API - First product with variants

--- src/services/Product/Product.Persistence/EntityConfigurations/ProductVariantConfiguration.cs
+++ src/services/Product/Product.Persistence/EntityConfigurations/ProductVariantConfiguration.cs
@@ -8,7 +8,7 @@ namespace Product.Persistence.EntityConfigurations;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using Product.Domain.Entities;
+using ProductVariantEntity = Domain.Entities.ProductVariantEntity;
 
 public class ProductVariantConfiguration : IEntityTypeConfiguration<ProductVariantEntity>
 {
@@ -17,5 +17,31 @@ public class ProductVariantConfiguration : IEntityTypeConfiguration<ProductVaria
         builder.ToTable("product-variant");
 
         builder.HasKey(pv => new { pv.Id, pv.ProductId, pv.ProductTypeId });
+
+        builder.HasData(
+               new ProductVariantEntity
+               {
+                   Id = Guid.Parse("bbf02f5f-6b6e-4e7e-a81d-fe504492b2bf"),
+                   ProductId = Guid.Parse("2346bc18-4a78-422a-b8db-33195ac3ed3f"),
+                   ProductTypeId = Guid.Parse("f4a6c9cc-cd6e-42d4-87be-f83e2cf2826b"),
+                   Price = 9.99m,
+                   OriginalPrice = 19.99m
+               },
+               new ProductVariantEntity
+               {
+                   Id = Guid.Parse("30a38fb1-ab79-447e-9f4a-860c8bbd62b4"),
+                   ProductId = Guid.Parse("2346bc18-4a78-422a-b8db-33195ac3ed3f"),
+                   ProductTypeId = Guid.Parse("751287ba-53f6-4874-b7b2-dc7b39ff6e9f"),
+                   Price = 7.99m
+               },
+               new ProductVariantEntity
+               {
+                   Id = Guid.Parse("3969771f-5401-4076-9880-be48be42306d"),
+                   ProductId = Guid.Parse("2346bc18-4a78-422a-b8db-33195ac3ed3f"),
+                   ProductTypeId = Guid.Parse("14ad27fb-2e94-407b-8e52-3376dc8f25fc"),
+                   Price = 19.99m,
+                   OriginalPrice = 29.99m
+               }
+           );
     }
 }
---
+++ src/services/Product/Product.Persistence/Migrations/20221221192423_ProductVariantFirstProductData.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Product.Persistence.Migrations
+{
+    public partial class ProductVariantFirstProductData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product-variant",
+                columns: new[] { "Id", "ProductId", "ProductTypeId", "CreatedAt", "CreatedBy", "LastModifiedBy", "LastModifiedDate", "OriginalPrice", "Price" },
+                values: new object[,]
+                {
+                    { new Guid("30a38fb1-ab79-447e-9f4a-860c8bbd62b4"), new Guid("2346bc18-4a78-422a-b8db-33195ac3ed3f"), new Guid("751287ba-53f6-4874-b7b2-dc7b39ff6e9f"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, null, null, 0m, 7.99m },
+                    { new Guid("3969771f-5401-4076-9880-be48be42306d"), new Guid("2346bc18-4a78-422a-b8db-33195ac3ed3f"), new Guid("14ad27fb-2e94-407b-8e52-3376dc8f25fc"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, null, null, 29.99m, 19.99m },
+                    { new Guid("bbf02f5f-6b6e-4e7e-a81d-fe504492b2bf"), new Guid("2346bc18-4a78-422a-b8db-33195ac3ed3f"), new Guid("f4a6c9cc-cd6e-42d4-87be-f83e2cf2826b"), new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, null, null, 19.99m, 9.99m }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product-variant",
+                keyColumns: new[] { "Id", "ProductId", "ProductTypeId" },
+                keyValues: new object[] { new Guid("30a38fb1-ab79-447e-9f4a-860c8bbd62b4"), new Guid("2346bc18-4a78-422a-b8db-33195ac3ed3f"), new Guid("751287ba-53f6-4874-b7b2-dc7b39ff6e9f") });
+
+            migrationBuilder.DeleteData(
+                table: "product-variant",
+                keyColumns: new[] { "Id", "ProductId", "ProductTypeId" },
+                keyValues: new object[] { new Guid("3969771f-5401-4076-9880-be48be42306d"), new Guid("2346bc18-4a78-422a-b8db-33195ac3ed3f"), new Guid("14ad27fb-2e94-407b-8e52-3376dc8f25fc") });
+
+            migrationBuilder.DeleteData(
+                table: "product-variant",
+                keyColumns: new[] { "Id", "ProductId", "ProductTypeId" },
+                keyValues: new object[] { new Guid("bbf02f5f-6b6e-4e7e-a81d-fe504492b2bf"), new Guid("2346bc18-4a78-422a-b8db-33195ac3ed3f"), new Guid("f4a6c9cc-cd6e-42d4-87be-f83e2cf2826b") });
+        }
+    }
+}
