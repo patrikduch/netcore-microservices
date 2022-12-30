b98905def41877f6c21b4cc223de3dd7816edcca
patrikduch <duchpatrik@icloud.com>
2022-12-30 02:15:16 +0100

    Product.API - ProductService: source code cleanup

--- src/services/Product/Product.Persistence/Services/ProductService.cs
+++ src/services/Product/Product.Persistence/Services/ProductService.cs
@@ -18,23 +18,40 @@ using System.Collections.Generic;
 public class ProductService : IProductService
 {
     private readonly IProductReader _productReader;
-    
 
+    /// <summary>
+    /// Initializes a new instance of the <seealso cref="ProductService"/>.
+    /// </summary>
+    /// <param name="productReader">ProductReader dependency object.</param>
     public ProductService(IProductReader productReader)
     {
         _productReader = productReader;
     }
 
-    public  async Task<ProductDetailDto?> GetProductDetail(Guid productId)
+    /// <summary>
+    /// Get product detail.
+    /// </summary>
+    /// <param name="productId">Unique product identifier.</param>
+    /// <returns><seealso cref="ProductDetailDto"/> object.</returns>
+    public async Task<ProductDetailDto?> GetProductDetail(Guid productId)
     {
         return await _productReader.FetchProductDetail(productId);
     }
 
+    /// <summary>
+    /// Get list of all products without restrictions.
+    /// </summary>
+    /// <returns>Collection of <seealso cref="ProductDto"/> objects.</returns>
     public async Task<List<ProductDto>> GetProducts()
     {
         return await _productReader.FetchProducts();
     }
 
+    /// <summary>
+    /// Get list of products filtered by category url.
+    /// </summary>
+    /// <param name="categoryUrl">Url of selected product category.</param>
+    /// <returns>Collection of <seealso cref="ProductDto"/> objects.</returns>
     public async Task<List<ProductDto>> GetProductsByCategory(string categoryUrl)
     {
         return await _productReader.FetchProducts(categoryUrl);
