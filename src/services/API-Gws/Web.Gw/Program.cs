dbaeda5967fce6755aa45631d6ac43aade504d50
patrikduch <duchpatrik@icloud.com>
2023-01-10 07:03:56 +0100

    Web.GW - NET 6 project initialization

---
+++ src/services/API-Gws/Web.Gw/Program.cs
@@ -0,0 +1,16 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapControllers();
+await app.UseOcelot();
+
+app.Run();
