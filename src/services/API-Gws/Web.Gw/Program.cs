5e1ed0db53f61f2e085506494fc871676a8ea4e5
patrikduch <duchpatrik@icloud.com>
2023-01-13 15:29:00 +0100

    Web.Gw - CQRS setup for dev and prod environment

--- src/services/API-Gws/Web.Gw/Program.cs
+++ src/services/API-Gws/Web.Gw/Program.cs
@@ -9,7 +9,39 @@ IConfiguration configuration = new ConfigurationBuilder()
 
 builder.Services.AddOcelot(configuration);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicyRelease", builder =>
+        builder
+        .WithOrigins(
+
+            "http://20.23.74.87",
+            "http://shopwinner.org"
+        )
+        .AllowAnyMethod()
+        .AllowAnyHeader()
+        .AllowCredentials()
+    );
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicyDev", builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+});
+
 var app = builder.Build();
+
+bool isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development";
+
+if (isDevelopment)
+{
+    app.UseCors("CorsPolicyDev");
+    app.UseDeveloperExceptionPage();
+
+} else
+{
+    app.UseCors("CorsPolicyRelease");
+}
 
 app.MapControllers();
 await app.UseOcelot();
