21ce134f6c226df479c3cbea0f4b698e002ea620
patrikduch <duchpatrik@icloud.com>
2023-03-25 19:49:35 +0100

    feat: IdentityServer initial config

--- src/services/IdentityAuth/Program.cs
+++ src/services/IdentityAuth/Program.cs
@@ -1,5 +1,19 @@
+using IdentityAuth;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add required services for IdentityServer
+builder.Services.AddIdentityServer()
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryClients(Config.Clients)
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
+
+
+app.UseIdentityServer(); // Add IdentityServer middleware
+
 
 app.MapGet("/", () => "Hello World!");
 
