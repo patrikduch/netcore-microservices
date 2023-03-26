b0b736acaee1435faf3ce8de7cfa53cd2e5bd2c2
patrikduch <duchpatrik@icloud.com>
2023-03-26 03:08:31 +0200

    Web.GW - Combination of configuration test

--- src/services/API-Gws/Web.Gw/Program.cs
+++ src/services/API-Gws/Web.Gw/Program.cs
@@ -1,5 +1,7 @@
+using Newtonsoft.Json.Linq;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using System.Text;
 using Web.Gw.Constants;
 using Web.Gw.Extensions;
 
@@ -9,10 +11,37 @@ IConfiguration configuration = new ConfigurationBuilder()
                                 
                             .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", false, true)
                             .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.ProjectDetail.json", false, true)
-                            //.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.Product.json", false, true)
                             .Build();
 
-builder.Services.AddOcelot(configuration)
+IConfiguration configuration2 = new ConfigurationBuilder()
+
+    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", false, true)
+    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.Product.json", false, true)
+    .Build();
+
+
+// Combine the Routes sections from both configurations
+JObject config1Json = JObject.Parse(configuration.GetSection("Routes").Value);
+JArray config1Routes = (JArray)config1Json["Routes"];
+
+JObject config2Json = JObject.Parse(configuration2.GetSection("Routes").Value);
+JArray config2Routes = (JArray)config2Json["Routes"];
+
+config1Routes.Merge(config2Routes);
+
+
+
+// Create a new configuration containing the merged routes
+var combinedConfigurationBuilder = new ConfigurationBuilder();
+var combinedJson = new JObject(new JProperty("Routes", config1Routes));
+var combinedJsonStream = new MemoryStream(Encoding.UTF8.GetBytes(combinedJson.ToString()));
+
+combinedConfigurationBuilder.AddJsonStream(combinedJsonStream);
+IConfiguration combinedConfiguration = combinedConfigurationBuilder.Build();
+
+
+
+builder.Services.AddOcelot(combinedConfiguration)
     .AddKubernetesFixed();
 
 builder.Services.AddCors(options =>
