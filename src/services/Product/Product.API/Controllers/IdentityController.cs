dd629f99df80724b616934986fcf835ad404b6fb
patrikduch <duchpatrik@icloud.com>
2023-04-23 01:35:18 +0200

    Product.API - Arbitrary IdentityController for retrieving claims

---
+++ src/services/Product/Product.API/Controllers/IdentityController.cs
@@ -0,0 +1,24 @@
+﻿namespace Product.API.Controllers;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/v1/{controller}")]
+[ApiController]
+[Authorize]
+public class IdentityController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetClaims()
+    {
+
+        var user = User.Claims.Select(c => new
+        {
+            tzpe = c.Type,
+            value = c.Value
+        });
+
+
+        return new JsonResult(user);
+    }
+}
