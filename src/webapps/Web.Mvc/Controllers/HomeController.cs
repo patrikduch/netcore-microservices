6266f36962bd163a5e612ecd6c6937e86ceacb45
patrikduch <duchpatrik@icloud.com>
2023-04-22 11:36:49 +0200

    Web.Mvc - Redundant imports

--- src/webapps/Web.Mvc/Controllers/HomeController.cs
+++ src/webapps/Web.Mvc/Controllers/HomeController.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Web.Mvc.Models;
 
 namespace Web.Mvc.Controllers
