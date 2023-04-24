3c4ab1f6ff141393ac7f39f516ec4b8acb1074b8
patrikduch <duchpatrik@icloud.com>
2023-04-25 00:21:27 +0200

    IdentityAuth - AccountControler: security headers

--- src/services/IdentityAuth/Quickstart/Account/AccountController.cs
+++ src/services/IdentityAuth/Quickstart/Account/AccountController.cs
@@ -12,11 +12,7 @@ using IdentityServer4.Stores;
 using IdentityServer4.Test;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace IdentityServerHost.Quickstart.UI
 {
@@ -25,7 +21,7 @@ namespace IdentityServerHost.Quickstart.UI
     /// The login service encapsulates the interactions with the user data store. This data store is in-memory only and cannot be used for production!
     /// The interaction service provides a way for the UI to communicate with identityserver for validation and context retrieval
     /// </summary>
-    //[SecurityHeaders]
+    [SecurityHeaders]
     [AllowAnonymous]
     public class AccountController : Controller
     {
