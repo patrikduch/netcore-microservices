61bf4c6e13f20c16495fa6a3ecab8085a56e8d54
patrikduch <duchpatrik@icloud.com>
2023-04-30 06:48:44 +0200

    Web.Mvc - OpenId testing

--- src/webapps/Web.Mvc/Program.cs
+++ src/webapps/Web.Mvc/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddAuthentication(options =>
         options.ClientSecret = "secret";
         options.ResponseType = "code";
 
-        //options.RequireHttpsMetadata = false;
+        options.RequireHttpsMetadata = false;
 
         options.Scope.Add("openid");
         options.Scope.Add("profile");
