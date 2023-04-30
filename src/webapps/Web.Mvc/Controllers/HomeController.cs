923e51ec2bbe1cd0cab115f106b69e001fb0b565
patrikduch <duchpatrik@icloud.com>
2023-04-30 04:57:04 +0200

    Web.Mvc - testing the OpenId connect

--- src/webapps/Web.Mvc/Controllers/HomeController.cs
+++ src/webapps/Web.Mvc/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Web.Mvc.Models;
 
 namespace Web.Mvc.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
--- src/webapps/Web.Mvc/Program.cs
+++ src/webapps/Web.Mvc/Program.cs
@@ -1,7 +1,36 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// OpenId configuration
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    }).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+    {
+        options.Authority = "https://identity.shopwinner.org"; // identity server address
+        options.ClientId = "mvc_client";
+        options.ClientSecret = "secret";
+        options.ResponseType = "code";
+
+        options.RequireHttpsMetadata = false;
+
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+
+        options.SaveTokens = true;
+        options.GetClaimsFromUserInfoEndpoint = true;
+    });
+
+
+
 
 var app = builder.Build();
 
@@ -14,6 +43,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
