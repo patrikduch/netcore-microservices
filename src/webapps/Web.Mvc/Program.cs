bfd21767749855bede2eee48056c751598ef527d
patrikduch <duchpatrik@icloud.com>
2023-05-01 15:12:23 +0200

    Web.Mvc - ForwaredHeaders fix

--- src/webapps/Web.Mvc/Program.cs
+++ src/webapps/Web.Mvc/Program.cs
@@ -1,10 +1,19 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    // Everything what was configured implicitly, we need to reset.
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
 
 
 // OpenId configuration
@@ -49,6 +58,8 @@ builder.Services.AddAuthentication(options =>
     });
 
 var app = builder.Build();
+
+app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
