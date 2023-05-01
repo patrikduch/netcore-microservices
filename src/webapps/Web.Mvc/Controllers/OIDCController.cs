09b462ac7cd7e7f48c7c13da818a10dd6d437b07
patrikduch <duchpatrik@icloud.com>
2023-05-01 17:16:56 +0200

    Web.Mvc - Oidc controller handler

---
+++ src/webapps/Web.Mvc/Controllers/OIDCController.cs
@@ -0,0 +1,22 @@
+﻿namespace Web.Mvc.Controllers;
+
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+[Authorize]
+public class OidcController : Controller
+{
+    [HttpGet("/your-callback-path")]
+    public async Task<IActionResult> Callback()
+    {
+        var authenticateResult = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        if (!authenticateResult.Succeeded)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        return RedirectToAction("Privacy", "Home");
+    }
+}
