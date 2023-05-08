be6ff923587eed16d74fb097197dabbc80fb5df8
patrikduch <duchpatrik@icloud.com>
2023-05-08 22:28:01 +0200

    Web.Mvc - Config class update

--- src/webapps/Web.Mvc/Program.cs
+++ src/webapps/Web.Mvc/Program.cs
@@ -91,14 +91,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-
-
 }
 
-
 app.UseDeveloperExceptionPage();
-
-
 app.UseStaticFiles();
 
 app.UseRouting();
