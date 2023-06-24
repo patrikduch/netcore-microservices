af20797bec948328f3e6d32df4b7c99324c1a730
patrikduch <duchpatrik@icloud.com>
2023-06-24 20:53:31 +0200

    Web.Mvc - CodeIdTokenToken

--- src/webapps/Web.Mvc/Program.cs
+++ src/webapps/Web.Mvc/Program.cs
@@ -58,7 +58,7 @@ builder.Services.AddAuthentication(options =>
         options.GetClaimsFromUserInfoEndpoint = true;
 
         // OpenID flow to use
-        options.ResponseType = OpenIdConnectResponseType.CodeIdToken;
+        options.ResponseType = OpenIdConnectResponseType.CodeIdTokenToken;
 
 
         options.UseTokenLifetime = false;
