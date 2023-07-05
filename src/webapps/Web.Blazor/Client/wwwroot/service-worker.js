5cdf72222beafdd569b726d86638e1bd5db033af
patrikduch <duchpatrik@icloud.com>
2023-07-05 07:43:28 +0200

    Web.Blazor - CICD test

--- src/webapps/Web.Blazor/Client/wwwroot/service-worker.js
+++ src/webapps/Web.Blazor/Client/wwwroot/service-worker.js
@@ -1,4 +1,9 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+//self.addEventListener('fetch', () => { });
+
+// 👇 Add these line to accept the message from this library.
+self.addEventListener('message', event => {
+    if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
+});
