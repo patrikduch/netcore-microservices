27db69303aee193d7b6b7f9dd1f55abc0131716b
patrikduch <duchpatrik@icloud.com>
2023-07-16 19:05:49 +0200

    User.API  - IdentityController

--- src/services/User/User.API/Controllers/IdentityController.cs
+++ src/services/User/User.API/Controllers/IdentityController.cs
@@ -1,6 +1,24 @@
 ﻿namespace User.API.Controllers;
 
-public class IdentityController
-{
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
+
+[Route("api/v1/{controller}")]
+[ApiController]
+[Authorize]
+public class IdentityController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetClaims()
+    {
+        var user = User.Claims.Select(c => new
+        {
+            tzpe = c.Type,
+            value = c.Value
+        });
+
+
+        return new JsonResult(user);
+    }
 }
